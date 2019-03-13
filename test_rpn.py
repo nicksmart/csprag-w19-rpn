69e507eaf9a74223ee96104814050bccea488ac9
Nick Smart <npsmart@umich.edu>
2019-03-13 14:20:52 -0400

    Added extra check to test negative values

--- test_rpn.py
+++ test_rpn.py
@@ -18,3 +18,5 @@ class TestBasics(unittest.TestCase):
     def test_exponentiation(self):
         result = rpn.calculate("2 3 ^")
         self.assertEqual(8, result)
+        result = rpn.calculate("-2 3 ^")
+        self.assertEqual(-8, result)
